7db571f02110c882f78cbc6d7a378a2e6b4a7ad2
evgeniynokalevich <zplacesound@gmail.com>
2020-02-09 06:25:47 +0300

    changes

--- apps/categories/serializer.py
+++ apps/categories/serializer.py
@@ -9,37 +9,53 @@ import re
 class CategoriesSerializer(serializers.HyperlinkedModelSerializer):
     children = serializers.SerializerMethodField('find_property')
 
-    def extract_digits(self,str):
+    def do_same_length(self,category,children):
+        good_len =[]
+        if len(category) == len(children):
+            return children
+        else:
+            #do work here
+            cut_len = len(children) - len(category)
+            for _ in range(cut_len):
+                good_len = children.pop()
+            return good_len
+
+    def extract_digits(self,name_category):
         #extract digits for compare
-        string_for_compare = str(str)
+        string_for_compare = str(name_category)
         res = re.findall(r'\d+',string_for_compare)
         return res
 
     def compare_categories(self,posible_children,name_category):
         #find less for children
         res = []
-        category = extract_digits(name_category)
-        maybe_children = extract_digits(posible_children)
+        category = self.extract_digits(name_category)
+        maybe_children = self.extract_digits(posible_children)
+        #do maybe children good len for compare
+        children_for_compare = self.do_same_length(category,maybe_children)
+        print(children_for_compare)
         for category_digit in category:
-            for children_digits in maybe_children:
+            for children_digits in children_for_compare:
                 if category_digit < children_digits:
-                    return
+                    break
                 else:
+                    res.append(children_digits)
 
 
 
-    def find_children(self,obj):
+    def find_relation(self,obj):
+        res =[]
         name_category = obj.name
         #find all without current category
-        all_names = Categories.objects.filter(name = name)
-        
-        for names in all_names:
-            compare_categories(all_names,name_category)
-        return self.extract_digits(name)
+        all_names = Categories.objects.exclude(name = name_category)
+
+        for posible_children in all_names:
+            res.append(self.compare_categories(posible_children,name_category))
+        return res
 
 
     def find_property(self,obj):
-        return self.find_children(obj)
+        return self.find_relation(obj)
 
 
     class Meta:
